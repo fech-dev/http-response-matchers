9255a2544cf1e00d4a1ac90a7f0be41727950ea8
Federico Mameli <federico_mameli@yahoo.com>
2024-09-16 08:50:53 +0000

    test: move createJsonResponse to utils file

--- tests/utils.ts
+++ tests/utils.ts
@@ -3,3 +3,18 @@ export function getRandomElement<T>(arr: Array<T>) {
   const index = Math.floor(Math.random() * arr.length);
   return arr[index];
 }
+
+export function createJsonResponse<T>(body: T, options: ResponseInit = {}) {
+  if (!options.status) options.status = 200;
+  if (!options.statusText) options.statusText = "ok";
+  if (!options.headers) options.headers = {};
+
+  options.headers = {
+    ...options.headers,
+    "Content-Type": "application/json",
+  };
+
+  const response = new Response(JSON.stringify(body), options);
+
+  return response;
+}
